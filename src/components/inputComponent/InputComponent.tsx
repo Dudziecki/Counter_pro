9ec62aaf433397e1c8f48e88eca07f5a4604dcd1
Michael Dudzik <mkdudik15@gmail.com>
2025-01-23 20:44:20 +0300

    Added some styles to Input Component element

--- src/components/inputComponent/InputComponent.tsx
+++ src/components/inputComponent/InputComponent.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from 'react'
+import style from './InputComponent.module.css'
 
 
 type InputComponentProps = {
     label: string;
     value: number;
     onChange: (value: number) => void;
-    className: string;
+
     isError: boolean;
 };
 
@@ -13,18 +14,20 @@ export const InputComponent: React.FC<InputComponentProps> = ({
                                                                   label,
                                                                   value,
                                                                   onChange,
-                                                                  className,
+
                                                                   isError
                                                               }) => {
+
     return (
-        <div className={className}>
-            <label className={'label'}>{label}</label>
+        <div className={style.GroupLabelInput}>
+            <label className={style.label}>{label}</label>
             <input
+                className={style.Input}
                 type="number"
                 value={value}
                 onChange={(e) => onChange(Number(e.target.value))}
                 style={{
-                    border: isError ? '2px solid red' : '1px solid black',
+                    border: isError ? '2px solid red' : '2px solid #63dbfd',
                     backgroundColor: isError ? 'rgba(255, 0, 0, 0.1)' : 'white'
                 }}
             />
