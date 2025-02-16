125e556c803dba0e8c63326b1747593eb600e86e
Michael Dudzik <mkdudik15@gmail.com>
2025-02-16 22:44:57 +0300

    take out selectors functions

---
+++ src/counter-selectors.ts
@@ -0,0 +1,6 @@
+import {RootState} from "./app/store.ts";
+
+
+export const selectIncrementValue = (state: RootState) :number=> state.counter.value
+export const selectMaxValue = (state: RootState) :number=> state.counter.maxValue
+export const selectStartValue = (state: RootState) :number=> state.counter.startValue
