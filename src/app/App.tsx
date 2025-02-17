6b9ecb829f3721429491e35313446010ed0076e3
Michael Dudzik <mkdudik15@gmail.com>
2025-02-17 22:26:28 +0300

    removing unnecessary props and functions

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,26 +1,24 @@
-import {useEffect, useState} from 'react';
-import style from './App.module.css';
+import {useEffect} from 'react'
+import style from './App.module.css'
 import {Counter} from "../components/counter/Counter.tsx";
-import {selectIncrementValue, selectMaxValue, selectStartValue} from "../counter-selectors.ts";
-import {useAppSelector} from "../useAppSelector.ts";
-import {useAppDispatch} from "../useAppDispatch.ts";
-import {changeIncrementValueAC, resetIncrementValueAC, setValuesAC} from "../counter-reducer.ts";
-import {setErrorAC, setSetBtnDisabledAC, toggleEditModeAC} from "../ui-reducer.ts";
-import {selectButtonsDisabled, selectSetBtn} from "../ui-selectors.ts";
+import {selectIncrementValue, selectMaxValue, selectStartValue} from "../counter-selectors.ts"
+import {useAppSelector} from "../useAppSelector.ts"
+import {useAppDispatch} from "../useAppDispatch.ts"
+import {changeIncrementValueAC, resetIncrementValueAC} from "../counter-reducer.ts"
+import {selectButtonsDisabled, selectSetBtn} from "../ui-selectors.ts"
 
 
 function App() {
-    const value = useAppSelector(selectIncrementValue);
-    const maxValue = useAppSelector(selectMaxValue);
+    const value = useAppSelector(selectIncrementValue)
+    const maxValue = useAppSelector(selectMaxValue)
     const startValue = useAppSelector(selectStartValue)
     const isSetDisabled = useAppSelector(selectSetBtn)
     const isButtonsDisabled = useAppSelector(selectButtonsDisabled)
+
     const dispatch = useAppDispatch()
 
-
-
-    let isResetDisabled = value === startValue || isButtonsDisabled || !isSetDisabled;
-    let isIncDisabled = value === maxValue || isButtonsDisabled || !isSetDisabled;
+    const isResetDisabled = value === startValue || isButtonsDisabled || !isSetDisabled
+    const isIncDisabled = value === maxValue || isButtonsDisabled || !isSetDisabled
 
     const onIncBtnClickHandler = () => {
         if (value < maxValue) {
@@ -30,24 +28,11 @@ function App() {
 
     useEffect(() => {
         localStorage.setItem('Increment Value', JSON.stringify(value));
-    }, [value]);
+    }, [value])
 
-    const onResBtnClickHandler = () => {
-        dispatch(resetIncrementValueAC())
-    };
-
-    const startValueClickHandler = () => {
-        // setIsEditMode(!isEditMode)
-        dispatch(toggleEditModeAC())
-        if (startValue < 0 || maxValue < 0 || startValue >= maxValue) return
-        dispatch(setValuesAC({startValue, maxValue}))
-        // setIsSetDisabled(true)
-        dispatch(setSetBtnDisabledAC({isSetDisabled: true}))
-    }
-
+    const onResBtnClickHandler = () => dispatch(resetIncrementValueAC())
     return (
         <div className={style.App}>
-
             <Counter
                 value={value}
                 maxValue={maxValue}
@@ -57,15 +42,9 @@ function App() {
                 isIncDisabled={isIncDisabled}
                 isSet={isSetDisabled}
                 startValue={startValue}
-                // isError={isError}
-                // setStartValue={setStartValue}
-                // setMaxValue={setMaxValue}
-                onClickValue={startValueClickHandler}
-                // isEditMode={isEditMode}
-                // setIsEditMode={setIsEditMode}
             />
         </div>
     );
 }
 
-export default App;
+export default App
