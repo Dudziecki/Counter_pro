381aa64d7c94e871761ede4c7f9fe19e50a7a72f
Michael Dudzik <mkdudik15@gmail.com>
2025-02-17 21:57:57 +0300

    rewriting my ui states into reducers,removing  unnecessary useeffects

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,71 +5,45 @@ import {selectIncrementValue, selectMaxValue, selectStartValue} from "../counter
 import {useAppSelector} from "../useAppSelector.ts";
 import {useAppDispatch} from "../useAppDispatch.ts";
 import {changeIncrementValueAC, resetIncrementValueAC, setValuesAC} from "../counter-reducer.ts";
+import {setErrorAC, setSetBtnDisabledAC, toggleEditModeAC} from "../ui-reducer.ts";
+import {selectButtonsDisabled, selectSetBtn} from "../ui-selectors.ts";
 
 
 function App() {
     const value = useAppSelector(selectIncrementValue);
     const maxValue = useAppSelector(selectMaxValue);
-    const startValue = useAppSelector(selectStartValue);
+    const startValue = useAppSelector(selectStartValue)
+    const isSetDisabled = useAppSelector(selectSetBtn)
+    const isButtonsDisabled = useAppSelector(selectButtonsDisabled)
     const dispatch = useAppDispatch()
 
 
-    const [isSetDisabled, setIsSetDisabled] = useState(true);
-    const [isError, setIsError] = useState(false);
-    const [isButtonsDisabled, setIsButtonsDisabled] = useState(false);
-    const [isEditMode, setIsEditMode] = useState(true)
-
-    // useEffect(() => {
-    //   const storedStartValue = localStorage.getItem('Start Value');
-    //   const storedMaxValue = localStorage.getItem('Max Value');
-    //
-    //   if (
-    //       storedStartValue !== null && storedMaxValue !== null &&
-    //       (startValue !== JSON.parse(storedStartValue) || maxValue !== JSON.parse(storedMaxValue))
-    //   ) {
-    //
-    //   }
-    // }, [startValue, maxValue]);
-
-    useEffect(() => {
-        if (startValue < 0 || maxValue < 0 || startValue >= maxValue) {
-            setIsError(true);
-            setIsSetDisabled(false);
-            setIsButtonsDisabled(true);
-        } else {
-            setIsError(false);
-            setIsSetDisabled(true);
-            setIsButtonsDisabled(false);
-        }
-    }, [startValue, maxValue]);
 
     let isResetDisabled = value === startValue || isButtonsDisabled || !isSetDisabled;
     let isIncDisabled = value === maxValue || isButtonsDisabled || !isSetDisabled;
 
     const onIncBtnClickHandler = () => {
         if (value < maxValue) {
-
             dispatch(changeIncrementValueAC())
         }
-    };
+    }
 
     useEffect(() => {
         localStorage.setItem('Increment Value', JSON.stringify(value));
     }, [value]);
 
     const onResBtnClickHandler = () => {
-
         dispatch(resetIncrementValueAC())
     };
 
     const startValueClickHandler = () => {
-        setIsEditMode(!isEditMode)
-        if (startValue < 0 || maxValue < 0 || startValue >= maxValue) return;
+        // setIsEditMode(!isEditMode)
+        dispatch(toggleEditModeAC())
+        if (startValue < 0 || maxValue < 0 || startValue >= maxValue) return
         dispatch(setValuesAC({startValue, maxValue}))
-        // setValue(startValue);
-        setIsSetDisabled(true);
-        // setStartValue(newStartValue) newStartValue, newMaxValue
-    };
+        // setIsSetDisabled(true)
+        dispatch(setSetBtnDisabledAC({isSetDisabled: true}))
+    }
 
     return (
         <div className={style.App}>
@@ -83,12 +57,12 @@ function App() {
                 isIncDisabled={isIncDisabled}
                 isSet={isSetDisabled}
                 startValue={startValue}
-                isError={isError}
+                // isError={isError}
                 // setStartValue={setStartValue}
                 // setMaxValue={setMaxValue}
                 onClickValue={startValueClickHandler}
-                isEditMode={isEditMode}
-                setIsEditMode={setIsEditMode}
+                // isEditMode={isEditMode}
+                // setIsEditMode={setIsEditMode}
             />
         </div>
     );
