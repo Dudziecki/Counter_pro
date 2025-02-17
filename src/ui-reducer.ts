d496d34da32b4f9c3806b7d5a985d68efde50eeb
Michael Dudzik <mkdudik15@gmail.com>
2025-02-17 22:16:57 +0300

    created Action Creaters and Reducer, adding new cases

--- src/ui-reducer.ts
+++ src/ui-reducer.ts
@@ -1,35 +1,36 @@
-import {createReducer} from "@reduxjs/toolkit";
+import {createAction, createReducer} from "@reduxjs/toolkit"
 
-// Определяем тип состояния UI
-interface UIState {
-    isEditMode: boolean;
-    isError: boolean;
-    isSetDisabled: boolean;
-    isButtonsDisabled: boolean;
+export type UIState = {
+    isEditMode: boolean
+    isError: boolean
+    isSetDisabled: boolean
+    isButtonsDisabled: boolean
 }
 
-// Начальное состояние
 const initialState: UIState = {
     isEditMode: true,
     isError: false,
     isSetDisabled: true,
     isButtonsDisabled: false,
-};
+}
 
+export const toggleEditModeAC = createAction('ui/toggleEditMode')
+export const setErrorAC = createAction<{ isError: boolean }>('ui/setError')
+export const setSetBtnDisabledAC = createAction<{ isSetDisabled: boolean }>('ui/setSetBtnDisabled')
+export const setButtonsDisabledAC = createAction<{ isButtonsDisabled: boolean }>('ui/setButtonsDisabled')
 
-// Создаем редьюсер
 export const uiReducer = createReducer(initialState, (builder) => {
     builder
-        .addCase("ui/toggleEditMode", (state) => {
+        .addCase(toggleEditModeAC, (state) => {
             state.isEditMode = !state.isEditMode;
         })
-        // .addCase("ui/setError", (state, action: PayloadAction<boolean>) => {
-        //     state.isError = action.payload;
-        // })
-        // .addCase("ui/setSetDisabled", (state, action: PayloadAction<boolean>) => {
-        //     state.isSetDisabled = action.payload;
-        // })
-        // .addCase("ui/setButtonsDisabled", (state, action: PayloadAction<boolean>) => {
-        //     state.isButtonsDisabled = action.payload;
-        // });
+        .addCase(setErrorAC, (state, action) => {
+            state.isError = action.payload.isError;
+        })
+        .addCase(setSetBtnDisabledAC, (state, action) => {
+            state.isSetDisabled = action.payload.isSetDisabled;
+        })
+        .addCase(setButtonsDisabledAC, (state, action) => {
+            state.isButtonsDisabled = action.payload.isButtonsDisabled
+        });
 });
