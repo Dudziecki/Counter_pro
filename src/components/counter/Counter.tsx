72bc90e8b2d7d9595d1193414645dc1db63391b4
Michael Dudzik <mkdudik15@gmail.com>
2025-02-17 22:06:30 +0300

    adding useeffects and deleting props

--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -1,9 +1,12 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Button} from "../button/Button.tsx";
 import style from "./counter.module.css";
 import {EditMode} from "../editMode/EditMode.tsx";
 import {setValuesAC} from "../../counter-reducer.ts";
 import {useAppDispatch} from "../../useAppDispatch.ts";
+import {setButtonsDisabledAC, setErrorAC, setSetBtnDisabledAC, toggleEditModeAC} from "../../ui-reducer.ts";
+import {useAppSelector} from "../../useAppSelector.ts";
+import {selectEditMode} from "../../ui-selectors.ts";
 
 
 type CounterPropsType = {
@@ -15,13 +18,7 @@ type CounterPropsType = {
     isIncDisabled?: boolean
     isSet: boolean
     startValue: number
-    isError: boolean
-    // setStartValue: (startValue: number) => void;
-    // setMaxValue: (maxValue: number) => void;
-    onClickValue: () => void;
-    isEditMode: boolean
-    setIsEditMode:(isEditMode: boolean)=>void
-}
+  }
 export const Counter: React.FC<CounterPropsType> = ({
                                                         value,
                                                         maxValue,
@@ -31,27 +28,38 @@ export const Counter: React.FC<CounterPropsType> = ({
                                                         isIncDisabled,
                                                         isSet,
                                                         startValue,
-                                                        isError,
-                                                        // setStartValue,
-                                                        // setMaxValue,
-                                                        onClickValue,
-                                                        isEditMode,
-                                                        setIsEditMode
+
                                                     }) => {
+    const isEditMode = useAppSelector(selectEditMode)
     const dispatch = useAppDispatch()
 
     const [newStartValue, setNewStartValue] = useState(startValue);
     const [newMaxValue, setNewMaxValue] = useState(maxValue);
     const setFunc = () => {
-        setIsEditMode(!isEditMode)
-        dispatch(setValuesAC({startValue:newStartValue, maxValue:newMaxValue}))
+        // setIsEditMode(!isEditMode)
+        dispatch(toggleEditModeAC())
+        dispatch(setValuesAC({startValue: newStartValue, maxValue: newMaxValue}))
     }
+    useEffect(() => {
+        if (newStartValue < 0 || newMaxValue < 0 || newStartValue >= newMaxValue) {
+            dispatch(setErrorAC({isError: true}))
+            dispatch(setSetBtnDisabledAC({isSetDisabled: false}))
+            // setIsButtonsDisabled(true);
+            dispatch(setButtonsDisabledAC({isButtonsDisabled:true}))
+        } else {
+
+            dispatch(setErrorAC({isError: false}))
+            dispatch(setSetBtnDisabledAC({isSetDisabled: true}))
+            // setIsButtonsDisabled(false);
+            dispatch(setButtonsDisabledAC({isButtonsDisabled:false}))
+        }
+    }, [newStartValue, newMaxValue]);
     return (
         <div className={style.Counter}>
             <EditMode isEditMode={isEditMode}
                       value={value}
                       startValue={startValue}
-                      isError={isError}
+                // isError={isError}
                       setStartValue={setNewStartValue}
                       setMaxValue={setNewMaxValue}
                       maxValue={maxValue}
