697d090a02b78bbbb5bcd7b58f4a0989a91f7d67
Michael Dudzik <mkdudik15@gmail.com>
2025-03-06 00:39:47 +0300

    removing this files and relocate func selectors into slices

--- src/features/counter/model/counter-selectors.ts
+++
@@ -1,5 +0,0 @@
-import {RootState} from "../../../app/store.ts"
-
-export const selectIncrementValue = (state: RootState) :number=> state.counter.value
-export const selectMaxValue = (state: RootState) :number=> state.counter.maxValue
-export const selectStartValue = (state: RootState) :number=> state.counter.startValue
