a1eb59a5d1044c97a918e7d11855ad2620c9f6d5
Michael Dudzik <mkdudik15@gmail.com>
2025-03-06 21:08:33 +0300

    adding thunkcreaters

--- src/features/counter/model/counterSlice.ts
+++ src/features/counter/model/counterSlice.ts
@@ -1,6 +1,6 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
 
-export type CounterState ={
+export type CounterState = {
     value: number;
     startValue: number;
     maxValue: number;
@@ -15,7 +15,7 @@ export enum CounterDefaults {
 
 const initialState: CounterState = {
     value: CounterDefaults.IncrementValue,
-    startValue:CounterDefaults.StartValue,
+    startValue: CounterDefaults.StartValue,
     maxValue: CounterDefaults.MaxValue,
 };
 
@@ -42,15 +42,45 @@ export const counterSlice = createSlice({
             // localStorage.setItem("Max Value", JSON.stringify(state.maxValue));
         }),
     }),
-    selectors:{
-        selectIncrementValue:(state):number=>state.value,
-        selectMaxValue:(state):number=>state.maxValue,
-        selectStartValue:(state):number=>state.startValue
+    extraReducers: (builder) => {
+        builder.addCase(loadCounterStateThunk.fulfilled, (state, action) => {
+            state.startValue = action.payload.startValue;
+            state.maxValue = action.payload.maxValue;
+            state.value = action.payload.value;
+        });
+    },
+    selectors: {
+        selectIncrementValue: (state): number => state.value,
+        selectMaxValue: (state): number => state.maxValue,
+        selectStartValue: (state): number => state.startValue
     }
 });
-export const {changeIncrementValueAC,resetIncrementValueAC,setValuesAC}=counterSlice.actions
-export const {selectIncrementValue,selectMaxValue,selectStartValue}=counterSlice.selectors
-export const counterReducer=counterSlice.reducer
+
+export const loadCounterStateThunk = createAsyncThunk(
+    `${counterSlice.name}/loadState`,
+    async () => {
+        const savedStartValue = localStorage.getItem("Start Value")
+        const savedMaxValue = localStorage.getItem("Max Value")
+        const savedValue = localStorage.getItem("Increment Value")
+        return {
+            startValue: savedStartValue ? JSON.parse(savedStartValue) : CounterDefaults.StartValue,
+            maxValue: savedMaxValue ? JSON.parse(savedMaxValue) : CounterDefaults.MaxValue,
+            value: savedValue ? JSON.parse(savedValue) : CounterDefaults.IncrementValue,
+        }
+    }
+)
+export const saveCounterStateThunk = createAsyncThunk(
+    `${counterSlice.name}/saveState`,
+    async (_, {getState}) => {
+        const state = getState() as { counter: CounterState }
+        localStorage.setItem("Start Value", JSON.stringify(state.counter.startValue));
+        localStorage.setItem("Max Value", JSON.stringify(state.counter.maxValue));
+        localStorage.setItem("Increment Value", JSON.stringify(state.counter.value));
+    }
+)
+export const {changeIncrementValueAC, resetIncrementValueAC, setValuesAC} = counterSlice.actions
+export const {selectIncrementValue, selectMaxValue, selectStartValue} = counterSlice.selectors
+export const counterReducer = counterSlice.reducer
 
 
 
