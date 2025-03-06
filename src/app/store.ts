90a4fc7a00047eac720ac05f0651fe36fac3d328
Michael Dudzik <mkdudik15@gmail.com>
2025-03-06 21:08:11 +0300

    deleting subscriber to store for localstorage

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,24 +1,20 @@
 import {configureStore} from '@reduxjs/toolkit'
 import {counterReducer, counterSlice} from "../features/counter/model/counterSlice.ts";
 import {uiReducer, uiSlice} from "../features/counter/model/uiSlice.ts";
-import {loadState} from "../common/utils/loadFromLocalStorage.ts";
-import {saveState} from "../common/utils/saveInLocalstorage.ts";
 
 
 // объединение reducer'ов с помощью combineReducers
 
-const preloadedState = loadState()|| undefined;
+
 // создание store
 export const store = configureStore({
     reducer: {
         [counterSlice.name]:counterReducer,
         [uiSlice.name]:uiReducer,
     },
-    preloadedState,
+
 })
-store.subscribe(() => {
-    saveState(store.getState());
-});
+
 
 // автоматическое определение типа всего объекта состояния
 export type RootState = ReturnType<typeof store.getState>
